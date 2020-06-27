d5b415bf420fa26d0f926526be2c637c5b551fe2
Felipe Rezende <feliperezende.srz@gmail.com>
2020-06-27 11:39:48 -0300

    Inserindo o analógico no controle

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,16 +3,16 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { AnalogControllerModule } from 'analog-controller';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    AnalogControllerModule
   ],
   declarations: [HomePage]
 })
